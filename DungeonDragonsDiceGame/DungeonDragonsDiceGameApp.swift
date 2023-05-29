7267353c6fbdc619ba691558e5a0b6443c7b0c85
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-05-29 16:25:09 -0400

    Initial Commit

---
+++ DungeonDragonsDiceGame/DungeonDragonsDiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDragonsDiceGameApp.swift
+//  DungeonDragonsDiceGame
+//
+//  Created by Lori Rothermel on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDragonsDiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
